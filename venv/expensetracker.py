955d8fd5436af8197224147b0d0a54dd8dddfcc7
Hannah Chiamaka Ejimofor <hannahejimofor@Hannahs-MacBook-Pro-2.local>
2026-01-15 21:45:25 +0100

    add validations

--- venv/expensetracker.py
+++ venv/expensetracker.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+
 def expenseTrackerCalc():
     print("Welcome to the Expense Tracker!")
     command = input("Enter a command (add, view, exit): ")
@@ -8,12 +11,24 @@ def expenseTrackerCalc():
             if not amount.replace('.', '', 1).isdigit():
                 print("Invalid amount. Please enter a numeric value.")
                 continue
+            if not amount:
+                print("Amount cannot be empty.")
+                continue
             category = input("Enter category: ")
+            if not category:
+                print("Category cannot be empty.")
+                continue
+            date = input("Enter date (YYYY-MM-DD): ")
+            if not date:
+                print(f"Date will be set to today's date.")
+                date = date.today()
+                continue
             item = {}
             item['amount'] = amount
             item['category'] = category
+            item['date'] = date
             allExpenses.append(item)
-            print(f"Added expense: {amount} in category {category}")
+            print(f"Added expense: {amount} in category {category} on {date}")
         elif command == "view":
             for expense in allExpenses:
                 totalExpenseAmount = sum(float(expense['amount']) for expense in allExpenses)
